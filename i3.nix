c0a52f19a8ac7d01d5d0d77fb6982527020a414d
Ard-Jan van Etten <ajvanetten@deschutse.nu>
2022-09-20 14:48:20 +0200

    s

---
+++ i3.nix
@@ -0,0 +1,76 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  
+  networking.hostName = "leaf";
+  networking.networkmanager.enable = true;
+  networking.useDHCP = false;
+
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = false;
+  
+  services.mingetty.autologinUser = "arte";
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+   environment.systemPackages = with pkgs; [
+    wget vim sway alacritty neofetch git godot bluez-alsa bluez bluez-tools networkmanager grim slurp wl-clipboard mesa radeontop htop libGL ntfs3g j4-dmenu-desktop brightnessctl xwayland brave pavucontrol steam discord lm_sensors micro vscode nano emacs bc chromium playerctl nodejs firefox spidermonkey minetest superTuxKart android-studio lutris imagemagick jetbrains.idea-community xfce.xfce4-whiskermenu-plugin wine winetricks powershell atom sublime3 xfce.xfce4-battery-plugin zerotierone pulseeffects gimp nmap google-chrome arandr kitty unzip zip gnome3.gnome-tweaks blender jdk11 sshfs transmission-gtk vlc gjs nmap metasploit dmenu flameshot xmobar cool-retro-term hsetroot feh
+  ];
+   
+   nixpkgs.config.permittedInsecurePackages = [
+     "openssl-1.0.2u"
+   ];  
+   
+  services.flatpak.enable = true;
+  xdg.portal.enable = true;
+  services.blueman.enable = true;
+  programs.adb.enable = true;
+
+  networking.firewall.allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+  networking.firewall.allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
+
+  sound.enable = true;
+  hardware.bluetooth.enable = true;
+  hardware.opengl.enable = true;
+
+  hardware.pulseaudio = {
+	enable = true;
+	package = pkgs.pulseaudioFull;
+  };
+
+  users.users.arte = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "adbusers" ];
+  };
+
+  hardware.opengl.driSupport32Bit = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
+  #services.xserver.desktopManager.xfce.enable = true;
+
+  services.xserver.displayManager.gdm.autoLogin.enable = true;
+  services.xserver.displayManager.gdm.autoLogin.user = "arte";
+  services.xserver.displayManager.defaultSession = "gnome";
+  
+  system.stateVersion = "20.03"; #no change
+}
